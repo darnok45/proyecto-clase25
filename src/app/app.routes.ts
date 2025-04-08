835f9418ff425f1a52721d7d366fae654b63576e
darnok45 <parragenaro429@gmail.com>
2025-04-08 11:01:42 -0300

    creacion de producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { FormularioComponent } from './contacto/formulario/formulario.component';
+import { ProductoComponent } from './productos/producto/producto.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/inicio', pathMatch:'full'},
@@ -8,4 +9,6 @@ export const routes: Routes = [
     {path:'inicio',component:InicioComponent},
 
     {path:'Contacto',component:FormularioComponent},
+
+    {path: 'producto',component:ProductoComponent}
 ];
