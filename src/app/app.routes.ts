157a9f159168a567924b5c5776ff811809cc2369
darnok45 <parragenaro429@gmail.com>
2025-04-08 08:39:03 -0300

    primer commit del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio/inicio.component';
+import { FormularioComponent } from './contacto/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'/inicio', pathMatch:'full'},
+
+    {path:'inicio',component:InicioComponent},
+
+    {path:'Contacto',component:FormularioComponent},
+];
